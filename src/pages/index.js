967312e67736832599f0fc531901ab8004f36b07
Elizeu Junior <77870830+ermyjr@users.noreply.github.com>
2023-03-15 17:13:08 -0400

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Isaque | Front-End Developer</title>
+        <title>ONG</title>
         <meta name="description" content="Sou um Desenvolvedor Front-End com experiência na construção de projetos e-commerce." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="./favicon-32.png" />
-      </Head>
+             </Head>
       <Main/>
     </div>
   );
