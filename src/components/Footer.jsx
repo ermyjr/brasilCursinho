194c53799d48c72baaec524e8b2436218a0c84a6
Elizeu Junior <77870830+ermyjr@users.noreply.github.com>
2023-03-14 22:02:25 -0400

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+      <footer>
+        <p>Copyright © 2023 
+          <a href="https://www.example.com/">Example Company</a>. All rights reserved.
+        </p>
+      </footer>
+    )
+  }
+  
+  export default Footer
